73c429a3e7cf6d8bbbb1f01b543b60bb0ea59224
j <urls3921@gmail.com>
2025-07-29 10:43:22 -0400

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,7 @@
 import Link from "next/link";
-import { RegisterLink, LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { buttonVariants } from "./ui/button";
+
 
 export async function Navbar() {
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
 
   return (
     <nav className="py-5 px-6 bg-gray-800 text-white flex items-center justify-between">
@@ -31,17 +27,7 @@ export async function Navbar() {
         </div>
       </div>
 
-      {/* {user ? (
-        <div className="flex items-center gap-4">
-          <p>{user.given_name}</p>
-          <LogoutLink className={buttonVariants({ variant: "secondary" })}>Logout</LogoutLink>
-        </div>
-      ) : (
-        <div className="flex items-center gap-6">
-          <LoginLink className={buttonVariants()}>Log in</LoginLink>
-          <RegisterLink className={buttonVariants()}>Sign up</RegisterLink>
-        </div>
-      )} */}
+
     </nav>
   );
 }
